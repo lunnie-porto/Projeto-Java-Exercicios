2e7221a7e12879c3b26c394edafcd8db0cfc9c9a
luana <luanamporto56@gmail.com>
2026-04-17 15:00:05 -0300

    ajuste no exercicio de notas

--- src/projetoJavaExercicios/notas.java
+++ src/projetoJavaExercicios/notas.java
@@ -1,10 +1,31 @@
 package projetoJavaExercicios;
 
+import java.util.Scanner;
+
 public class notas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner leia = new Scanner(System.in);
+		float nota1, nota2, nota3, nota4, media;
+		
+		System.out.println("Digite a Nota 1:");
+		nota1 = leia.nextFloat();
+		
+		System.out.println("Digite a Nota 2:");
+		nota2 = leia.nextFloat();
+		
+		System.out.println("Digite a Nota 3:");
+		nota3 = leia.nextFloat();
+		
+		System.out.println("Digite a Nota 4:");
+		nota4 = leia.nextFloat();
+		
+		media = (nota1 + nota2 + nota3 + nota4) /4;
+		
+		System.out.println("Média final: " + media);
+		
 	}
 
 }
